d957e6ea73bd8344baff150529ecfcef812deaf3
Riccardo Accomando Emanuele <riccardoaccoma@gmail.com>
2021-09-16 17:27:32 +0200

    Changes

--- 00/ex00/src/megaphone.cpp
+++ 00/ex00/src/megaphone.cpp
@@ -11,7 +11,7 @@ int	main(int argc, char **argv) {
 
 	for (int i = 1; i < argc; i++)
 		msg.append(argv[i]);
-	for (char c : msg)
-		std::cout << (char)toupper(c);
+	for (int i = 0; i < msg.length(); i++)
+		std::cout << (char)toupper(msg[i]);
 	std::cout << std::endl;
 }
